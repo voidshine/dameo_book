f818f1b2870885f157a5945b389349ee13dc24c3
voidshine <skylark@nightshade>
2025-12-20 09:12:55 -0800

    Convert and trim spaces; add game record

--- generate.sh
+++ generate.sh
@@ -1,1 +1,1 @@
-cat Introduction.md Game01.md Game02.md Game03.md | pandoc -f markdown -o book.html
+cat Introduction.md Game01.md Game02.md Game03.md Game04.md Game05.md | pandoc -f markdown -o book.html
